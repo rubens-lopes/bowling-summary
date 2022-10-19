65dd84d397d39cd0251c2b731192034a5b49e88c
Rubens Armôa Lopes <rubens@L3-000459.lambda3.com.br>
2022-10-19 18:17:54 -0400

    Rubens

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 fn joga(jogo: &str) -> u16 {
-    let mut chars = jogo.chars();
     let mut total: u16 = 0;
-    if let Some(car) = chars.next() {}
+    let turnos = jogo.split_whitespace();
+
+    for turno in turnos {
+        let mut chars = turno.chars();
+
+        while let Some(car) = chars.next() {
+            total += car.to_string().parse::<u16>().unwrap();
+        }
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -16,5 +25,15 @@ mod tests {
     #[test]
     fn uma_jogada_sem_derrubar_tudo() {
         assert_eq!(3, joga("12"));
+    }
+
+    #[test]
+    fn duas_jogadas_sem_derrubar_tudo() {
+        assert_eq!(10, joga("32 23"));
+    }
+
+    #[test]
+    fn uma_jogada_sem_derrubar_nada() {
+        assert_eq!(0, joga("--"));
     }
 }
